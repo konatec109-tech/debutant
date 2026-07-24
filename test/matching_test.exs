50591f8810380d6209eecea6c205cfd6ffa2c703
konatec109-tech <konatec109@gmail.com>
2026-07-24 15:13:57 +0000

    insufficient fund handler

--- test/matching_test.exs
+++ test/matching_test.exs
@@ -14,11 +14,13 @@ defmodule MatchingTest do
   test "test credit function in Elixir" do
     user_info = {:success, %{name: "ibrahim", age: 22, phone: "0704102697", balance_atomic: 50000}}
     assert credit(user_info, 1000) == {:success, %{name: "ibrahim", age: 22, phone: "0704102697", balance_atomic: 51000}}
-
   end
 
   test "testin debit function in Elixir" do
     user_info = {:success, %{name: "ibrahim", age: 22, phone: "0704102697", balance_atomic: 50000}}
     assert debit(user_info, 49500) == {:success, %{name: "ibrahim", age: 22, phone: "0704102697", balance_atomic: 500}}
+
+    assert debit(user_info, 100000) == {:error, "Insufficient funds. Your current balance is 50000."}
   end
+
 end
